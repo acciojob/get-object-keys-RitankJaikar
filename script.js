fb76a336b22daa55fdf9b8fbeea377989eaa0e23
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-25 20:34:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Create the student object with a name property
+const student = {
+  name: "John Doe"
+};
+
+// Add getKeys method to Object.prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Call the getKeys method on the student object
+console.log(student.getKeys()); // Output: ["name"]
